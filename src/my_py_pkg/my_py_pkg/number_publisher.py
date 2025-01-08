4b191627da8e0dc58fb6b8b89b85f88815e04fa3
Gunjan Yadav <gunjan.yadav@uni-ulm.de>
2025-01-08 18:14:10 +0100

    launch file

--- src/my_py_pkg/my_py_pkg/number_publisher.py
+++ src/my_py_pkg/my_py_pkg/number_publisher.py
@@ -1,32 +1,36 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
+
 from example_interfaces.msg import Int64
 
-class NumberPublisherNode(Node):  
+
+class NumberPublisherNode(Node):
     def __init__(self):
-        super().__init__("number_publisher") 
-        #self.declare_parameter("test123")            # declaring neww parameter
-        #self.declare_parameter("another_param") 
-        self.declare_parameter("number_to_publish",2)     #2 is default parameter
+        super().__init__("number_publisher")
+        self.declare_parameter("number_to_publish", 2)
+        self.declare_parameter("publish_frequency", 1.0)
 
         self.number_ = self.get_parameter("number_to_publish").value
-# robot_news_ ststion is a publisher which is publishing a message on robot_news topic
-        self.publisher_ =self.create_publisher(Int64, "number", 10)
-        self.timer_ = self.create_timer(0.5,self.publish_number)
-        self.get_logger().info("number publisher has been started.")
+        self.publish_frequency_ = self.get_parameter("publish_frequency").value
+
+        self.number_publisher_ = self.create_publisher(Int64, "number", 10)
+        self.number_timer_ = self.create_timer(
+            1.0 / self.publish_frequency_, self.publish_number)
+        self.get_logger().info("Number publisher has been started.")
 
     def publish_number(self):
         msg = Int64()
         msg.data = self.number_
+        self.number_publisher_.publish(msg)
 
-        self.publisher_.publish(msg)    
 
 def main(args=None):
     rclpy.init(args=args)
-    node = NumberPublisherNode()  
+    node = NumberPublisherNode()
     rclpy.spin(node)
     rclpy.shutdown()
 
-if __name__ == "__main__":
+
+if __name__ == "_main_":
     main()
